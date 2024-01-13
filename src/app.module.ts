49ffd87f0f1a545aad1c470b73e4ad5aaf68c19f
rudesoul <prabesh7@gmail.com>
2024-01-13 16:14:08 -0500

    - created endpoints to create new user, login user, to access admin dashboard and to access user profile - created user roles management - create local and JWT guards - create authentication strategy with JWT create local authentication strategy - create user service methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule, UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
